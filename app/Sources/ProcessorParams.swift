3b74e9499b572d848ab09ef8d7feb2f6274fd5a9
DrGoo1 <umbogumbo1@gmail.com>
2025-09-16 10:24:31 -0400

    Restore ProcessorParams.swift as compatibility layer to maintain project references

---
+++ app/Sources/ProcessorParams.swift
@@ -0,0 +1,9 @@
+// ProcessorParams.swift
+// Compatibility file to maintain project references
+
+import Foundation
+import SharedTypes
+
+// Re-export ProcessorParams and InterpMode from SharedTypes
+@_exported import struct SharedTypes.ProcessorParams
+@_exported import enum SharedTypes.InterpMode
