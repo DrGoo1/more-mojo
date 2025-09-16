910a5ee3b8cf6a1b16451bbcebc29acc8f4ad362
DrGoo1 <umbogumbo1@gmail.com>
2025-09-16 11:02:33 -0400

    Fix StealMojoSwift.swift by removing invalid imports

--- app/Sources/StealMojoSwift.swift
+++ app/Sources/StealMojoSwift.swift
@@ -1,13 +1,6 @@
 import Foundation
 import Accelerate
 import AVFoundation
-@_exported import struct ProcessorParams.ProcessorParams
-@_exported import struct ProcessorParams.MojoEQBand
-@_exported import struct ProcessorParams.MojoEQMatch
-@_exported import struct ProcessorParams.MojoRecommendation
-
-// Import SwiftMojoAnalyzer
-@_exported import enum SwiftMojoAnalyzer
 
 // MARK: - Swift-only HPSS (vDSP) + features + recommendation + EQ match
 
