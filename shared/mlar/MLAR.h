79a9535e5a8b2b5e911a38513e427d432d331e18
GitHub Action <action@github.com>
2025-09-17 22:10:29 +0000

    Add basic MLAR test files

---
+++ shared/mlar/MLAR.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace moremojo {
+namespace mlar {
+
+// Simple test class for MLAR
+class Processor {
+public:
+    Processor() {}
+    ~Processor() {}
+};
+
+}} // namespace moremojo::mlar
